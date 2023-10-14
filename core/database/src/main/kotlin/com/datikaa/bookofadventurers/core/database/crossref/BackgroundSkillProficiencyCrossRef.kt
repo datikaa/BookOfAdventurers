e294cf6cdee5d0145ee1afa245e4dc0b2d28e69d
daniel.dattler <daniel.dattler@bcsconsult.hu>
2023-10-14 16:23:57 +0200

    #37 Add SRD prefill

--- core/database/src/main/kotlin/com/datikaa/bookofadventurers/core/database/crossref/BackgroundSkillProficiencyCrossRef.kt
+++ core/database/src/main/kotlin/com/datikaa/bookofadventurers/core/database/crossref/BackgroundSkillProficiencyCrossRef.kt
@@ -3,8 +3,8 @@ package com.datikaa.bookofadventurers.core.database.crossref
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 
-@Entity(primaryKeys = ["backgroundsId", "modifierId"])
+@Entity(primaryKeys = ["backgroundId", "modifierId"])
 data class BackgroundSkillProficiencyCrossRef(
-    @ColumnInfo(index = true) val backgroundsId: Long,
+    @ColumnInfo(index = true) val backgroundId: Long,
     @ColumnInfo(index = true) val modifierId: Long
 )
