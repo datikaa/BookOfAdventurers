fc1c414ee73086f683bd2043030b1024ee018a11
Daniel Dattler <daniel.dattler@bv-group.com>
2024-12-20 21:04:44 +0100

    Fix previews in Fleet

--- build-logic/convention/src/main/kotlin/KmmComposeConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/KmmComposeConventionPlugin.kt
@@ -1,13 +1,28 @@
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.getByType
+import org.jetbrains.compose.ComposeExtension
 
 class KmmComposeConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
             with(pluginManager) {
+                apply("org.jetbrains.kotlin.multiplatform")
                 apply("org.jetbrains.kotlin.plugin.compose")
                 apply("org.jetbrains.compose")
             }
+
+            setUpComposePreviews()
         }
+    }
+
+    private fun Project.setUpComposePreviews() {
+        val composeExtension = extensions.getByType<ComposeExtension>()
+
+        val debugImplementation = project.configurations.findByName("debugImplementation")
+            ?: project.configurations.create("debugImplementation")
+
+        // if i'll ever need it: "org.jetbrains.compose.ui:ui-tooling:<version>"
+        project.dependencies.add(debugImplementation.name, composeExtension.dependencies.uiTooling)
     }
 }
